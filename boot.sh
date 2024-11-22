78bf66f4fd35de621719ea8b27977c33d7c20237
ryokpen87044 <106538588+ryokpen87044@users.noreply.github.com>
2024-11-22 15:59:07 +0900

    Edit boot.sh

--- boot.sh
+++ boot.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-# Command line arguments
+# Command line arguments and options
 args=${1:-TEST}
+
+while getopts pt OPT; do
+  case $OPT in
+     p) args="PRODUCT";;
+     t) args="TEST";;
+     *) args="TEST";;
+  esac
+done
 
 # Activate the virtual environment
 . .venv/bin/activate
