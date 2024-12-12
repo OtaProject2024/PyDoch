a52306a0e54eee18ffbd2286930c437a348e618e
ryokpen87044 <106538588+ryokpen87044@users.noreply.github.com>
2024-12-12 22:48:01 +0900

    Fix overview.py.

--- src/murdoch/mode/overview.py
+++ src/murdoch/mode/overview.py
@@ -102,7 +102,8 @@ class Overview:
         self.__draw_right(screen, start_y + 1, start_x + self.content_width + 3)
         self.__draw_footer(screen, start_y + self.min_height - 1, start_x)
         screen.refresh()
-        curses.napms(2000)
+        curses.napms(50)
+        # curses.napms(2000)
 
     def __simple_display(self, screen):
         screen.addstr(2, 0, f"time: {time.strftime('%Y/%m/%d %H:%M:%S', time.localtime())}")
